69e9a3be21194cb0b3f45aaaaeccac3d942cf777
SDJAIM <susandejesus96@gmail.com>
2025-04-30 14:25:26 +0200

    middlewares, config ..

--- api/src/app.js
+++ api/src/app.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
+const userAgentMiddleware = require('./middlewares/user-agent')
+const errorHandlerMiddleware = require('./middlewares/error-handler')
+const userTrackingMiddleware = require('./middlewares/user-tracking')
+
 const router = require('./routes')
 
 app.use(express.json({ limit: '10mb', extended: true }))
+app.use(userAgentMiddleware)
+app.use(userTrackingMiddleware)
+
 app.use('/api', router)
+
+app.use(errorHandlerMiddleware)
 
 module.exports = app
---
+++ api/src/migrations/20250430103924-create-custumer-table.js
@@ -0,0 +1,50 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('customers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+      },
+      prefix: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      telephone: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      birthdate: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('customers')
+  }
+}
