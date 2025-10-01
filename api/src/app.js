7e68f332965da55438cc6e3a51b8bc1e687b7e37
SDJAIM <susandejesus@gmail.com>
2025-10-01 15:40:16 +0200

    feat: integrate WebSocket service and middleware for Telegram bot functionality

--- api/src/app.js
+++ api/src/app.js
@@ -3,12 +3,14 @@ const app = express()
 const userAgentMiddleware = require('./middlewares/user-agent')
 const errorHandlerMiddleware = require('./middlewares/error-handler')
 const userTrackingMiddleware = require('./middlewares/user-tracking')
+const exposeServiceMiddleware = require('./middlewares/expose-services')
 
 const router = require('./routes')
 
 app.use(express.json({ limit: '10mb', extended: true }))
 app.use(userAgentMiddleware)
 app.use(userTrackingMiddleware)
+app.use(...Object.values(exposeServiceMiddleware))
 
 app.use('/api', router)
 
