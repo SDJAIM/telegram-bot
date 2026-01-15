2b6f70f20b0007724d75710cd29889fb15483d8e
SDJAIM <susandejesus@gmail.com>
2026-01-15 14:07:10 +0100

    feat(auth): implement user authentication controller and routes with session management

--- api/src/app.js
+++ api/src/app.js
@@ -1,12 +1,35 @@
 const express = require('express')
 const app = express()
+
+const errorHandler = require('./middlewares/error-handler')
 const userAgentMiddleware = require('./middlewares/user-agent')
-const errorHandlerMiddleware = require('./middlewares/error-handler')
 const userTrackingMiddleware = require('./middlewares/user-tracking')
 const exposeServiceMiddleware = require('./middlewares/expose-services')
+
 const { createClient } = require('redis')
+const session = require('express-session')
+const { RedisStore } = require('connect-redis')
+
 const redisClient = createClient({ url: process.env.REDIS_URL })
-const subscriberClient = createClient({ url: process.env.REDIS_URL })
+redisClient.connect().catch(console.error)
+const subscriberClient = redisClient.duplicate()
+subscriberClient.connect().catch(console.error)
+
+const sessionConfig = session({
+  store: new RedisStore({ client: redisClient }),
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: process.env.NODE_ENV === 'production',
+    httpOnly: true,
+    domain: new URL(process.env.API_URL).hostname,
+    path: '/',
+    sameSite: 'Lax',
+    maxAge: 1000 * 60 * 3600
+  }
+})
+
 require('./events')(redisClient, subscriberClient)
 
 app.use((req, res, next) => {
@@ -14,15 +37,15 @@ app.use((req, res, next) => {
   next()
 })
 
-const router = require('./routes')
+const routes = require('./routes')
 
+app.use(sessionConfig)
 app.use(express.json({ limit: '10mb', extended: true }))
-app.use(userAgentMiddleware)
 app.use(userTrackingMiddleware)
+app.use(userAgentMiddleware)
 app.use(...Object.values(exposeServiceMiddleware))
 
-app.use('/api', router)
-
-app.use(errorHandlerMiddleware)
+app.use('/api', routes)
+app.use(errorHandler)
 
 module.exports = app
